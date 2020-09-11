bc20260efca5b81b5cb39b2b8624f887a9e90986
Yuki-Shibanuma <smatosan@gmail.com>
2020-09-12 07:01:07 +0900

    init

---
+++ src/main/java/mckd/me/prog/Prog.java
@@ -0,0 +1,17 @@
+package mckd.me.prog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Prog extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
