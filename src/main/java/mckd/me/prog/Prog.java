c22b69c33a4387b7b5596b6ec4f54a2ab46b02d1
unknown <ataru4101826514@gmail.com>
2020-09-26 13:27:37 +0900

    README

--- src/main/java/mckd/me/prog/Prog.java
+++ src/main/java/mckd/me/prog/Prog.java
@@ -7,7 +7,7 @@ public final class Prog extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("hello");
     }
 
     @Override
