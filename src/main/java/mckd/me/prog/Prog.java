144c2ed067a94570ee48cf84d75101e402b13261
unknown <ataru4101826514@gmail.com>
2020-11-11 18:33:56 +0900

    create world

--- src/main/java/mckd/me/prog/Prog.java
+++ src/main/java/mckd/me/prog/Prog.java
@@ -1,6 +1,7 @@
 package mckd.me.prog;
 
 import mckd.me.prog.Worlds.MainWorld;
+import mckd.me.prog.Worlds.TestWorld;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Prog extends JavaPlugin {
@@ -10,6 +11,7 @@ public final class Prog extends JavaPlugin {
         // Plugin startup logic
         System.out.println("hello");
         new MainWorld(this);
+        new TestWorld(this);
     }
 
     @Override
