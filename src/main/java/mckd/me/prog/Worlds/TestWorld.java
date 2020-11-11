3b87c792e99a4348765501c1793db3b755c98057
unknown <ataru4101826514@gmail.com>
2020-11-11 18:42:26 +0900

    a

--- src/main/java/mckd/me/prog/Worlds/TestWorld.java
+++ src/main/java/mckd/me/prog/Worlds/TestWorld.java
@@ -1,10 +1,21 @@
 package mckd.me.prog.Worlds;
 
 import mckd.me.prog.Prog;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 
 public class TestWorld implements Listener {
-    public TestWorld(Prog plugin){
-        plugin.getServer().getPluginManager().registerEvents(this,plugin);
+    public TestWorld(Prog plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        if (e.getPlayer().getWorld().getName() .equals("test")) {
+            Player player = e.getPlayer();
+            player.sendMessage("Hello");
+        }
     }
 }
