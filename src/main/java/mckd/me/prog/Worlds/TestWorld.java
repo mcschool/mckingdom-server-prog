a0fd54f473f128c0a29d0bd66e9d0704df9babff
unknown <ataru4101826514@gmail.com>
2020-11-14 15:37:38 +0900

    a

--- src/main/java/mckd/me/prog/Worlds/TestWorld.java
+++ src/main/java/mckd/me/prog/Worlds/TestWorld.java
@@ -3,11 +3,13 @@ package mckd.me.prog.Worlds;
 import mckd.me.prog.Prog;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockCanBuildEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 
 public class TestWorld implements Listener {
     public TestWorld(Prog plugin) {
@@ -22,16 +24,20 @@ public class TestWorld implements Listener {
             player.sendMessage("Hello" + String.valueOf(count));
         }
     }
+
     @EventHandler
-    public void sendMessage(BlockBreakEvent e){
-        Player player = e.getPlayer();
-        World world =e.getPlayer().getWorld();
-        Location loc1 = player.getLocation();
-        Location loc2 = new Location(world, loc1.getX(),loc1.getY()+10,loc1.getZ());
-        player.teleport(loc2);
+    public void sendMessage(BlockBreakEvent e) {
+        if (e.getPlayer().getWorld().getName().equals("test")) {
+            Player player = e.getPlayer();
+            World world = e.getPlayer().getWorld();
+            Location loc1 = player.getLocation();
+            Location loc2 = new Location(world, loc1.getX(), loc1.getY() + 10, loc1.getZ());
+            player.teleport(loc2);
+        }
     }
-    private int getPlayer(Player player) {
-        int playerCount = player.getWorld().getPlayers().size();
-        return playerCount;
+
+        private int getPlayer (Player player){
+            int playerCount = player.getWorld().getPlayers().size();
+            return playerCount;
+        }
     }
-}
