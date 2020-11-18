bf60d2165b6594751b4062a14f55e8f60323cbd2
unknown <ataru4101826514@gmail.com>
2020-11-18 17:16:28 +0900

    update Tnt

--- src/main/java/mckd/me/prog/Worlds/TntWorld.java
+++ src/main/java/mckd/me/prog/Worlds/TntWorld.java
@@ -4,20 +4,24 @@ import mckd.me.prog.Prog;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedMainHandEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 
 public class TntWorld implements Listener {
+    public String worldName = "Tnt";
+    public Location startPlace ;
     public TntWorld(Prog plugin){
         plugin.getServer().getPluginManager().registerEvents(this,plugin);
+        this.startPlace = new Location(Bukkit.getWorld(this.worldName),-263,51,1088);
     }
     //待合所にテレポート
+    @EventHandler
     public void changeWorld(PlayerChangedWorldEvent e){
-        if(e.getPlayer().getWorld().getName().equals("Tnt")){
+        if(e.getPlayer().getWorld().getName().equals(this.worldName)){
             Player player = e.getPlayer();
-            Location location = new Location(Bukkit.getWorld("Tnt"),-263,51,1088);
-            player.teleport(location);
+            player.teleport(this.startPlace);
         }
     }
 }
