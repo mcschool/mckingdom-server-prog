71726786541ca69ef46127cdb4d475dea5d94932
unknown <ataru4101826514@gmail.com>
2020-11-18 18:39:22 +0900

    a

--- src/main/java/mckd/me/prog/Worlds/TntWorld.java
+++ src/main/java/mckd/me/prog/Worlds/TntWorld.java
@@ -79,11 +79,12 @@ public class TntWorld implements Listener {
         Player player = e.getPlayer();
         World world = player.getWorld();
         if(e.getPlayer().getWorld().getName().equals("tnt")){
-         Location location = e.getPlayer().getLocation().clone().subtract(0,-1,0);
-         Block block = location.getBlock();
-        if (block.getType() == Material.TNT){
-            player.sendMessage(block.getType().name());
-            world.getBlockAt(location).setType(Material.AIR);
+            player.sendMessage("a");
+            Location location = e.getPlayer().getLocation().clone().subtract(0,-1,0);
+            Block block = location.getBlock();
+            if (block.getType() == Material.TNT){
+                player.sendMessage(block.getType().name());
+                world.getBlockAt(location).setType(Material.AIR);
             }
         }
     }
