2cfb9ec44c43a31cc21c4fe726e8b92f67e434c4
unknown <ataru4101826514@gmail.com>
2020-11-18 18:18:30 +0900

    update tnt

--- src/main/java/mckd/me/prog/Worlds/TntWorld.java
+++ src/main/java/mckd/me/prog/Worlds/TntWorld.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.player.PlayerChangedMainHandEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -19,12 +20,14 @@ import java.util.List;
 public class TntWorld implements Listener {
     private Prog plugin;
     public String worldName = "tnt";
-    public Location startPlace ;
-    public TntWorld(Prog plugin){
+    public Location startPlace;
+
+    public TntWorld(Prog plugin) {
         this.plugin = plugin;
-        plugin.getServer().getPluginManager().registerEvents(this,plugin);
-        this.startPlace = new Location(Bukkit.getWorld(this.worldName),-263,51,1088);
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        this.startPlace = new Location(Bukkit.getWorld(this.worldName), -263, 51, 1088);
     }
+
     //待合所にテレポート
     @EventHandler
     public void changeWorld(PlayerChangedWorldEvent e) {
@@ -33,13 +36,14 @@ public class TntWorld implements Listener {
             player.teleport(this.startPlace);
         }
     }
+
     @EventHandler
     public void breakBlock(BlockBreakEvent e) {
         if (e.getPlayer().getWorld().getName().equals(this.worldName)) {
             Player player = e.getPlayer();
             Block block = e.getBlock();
             if (block.getType() == Material.GRASS) {
-                player.sendTitle("GameStart","ゲームスタート",20,20,20);
+                player.sendTitle("GameStart", "ゲームスタート", 20, 20, 20);
                 player.sendMessage("移動するよ");
                 this.startGame();
             } else {
@@ -48,17 +52,24 @@ public class TntWorld implements Listener {
         }
     }
 
-    public void startGame(){
+    public void startGame() {
         World world = Bukkit.getWorld("tnt");
         List<Player> players = world.getPlayers();
 
-        for(Player player:players){
-            new BukkitRunnable(){
+        for (Player player : players) {
+            new BukkitRunnable() {
                 @Override
-                public void run(){
-                    player.teleport(new Location(Bukkit.getWorld("tnt"),-265,40,1047));
+                public void run() {
+                    player.teleport(new Location(Bukkit.getWorld("tnt"), -265, 40, 1047));
                 }
             }.runTaskLater(this.plugin, 20);
+        }
+    }
+
+    @EventHandler
+    public void onBlockExplode(BlockExplodeEvent e) {
+        if (e.getBlock().getWorld().getName().equals("tnt")) {
+            e.setCancelled(true);
         }
     }
 }
