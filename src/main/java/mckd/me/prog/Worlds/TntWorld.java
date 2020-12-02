2150e1f41749e195a037604c402b02d8b8f554d0
unknown <ataru4101826514@gmail.com>
2020-12-02 19:31:35 +0900

    update tnt

--- src/main/java/mckd/me/prog/Worlds/TntWorld.java
+++ src/main/java/mckd/me/prog/Worlds/TntWorld.java
@@ -152,8 +152,8 @@ public class TntWorld implements Listener {
                 world.getBlockAt(location).setType(Material.GRASS);
             }
             location.add(-20, 0, 0);
-            location.add(0, 0, -20);
         }
+            location.add(0, 0, -20);
     }
 
         //ゲームクリア
