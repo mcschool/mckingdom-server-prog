1e7c107a01409811cfd0d892e80cea0e780f319a
unknown <ataru4101826514@gmail.com>
2020-12-02 16:48:23 +0900

    tnt update

--- src/main/java/mckd/me/prog/Worlds/TntWorld.java
+++ src/main/java/mckd/me/prog/Worlds/TntWorld.java
@@ -119,12 +119,9 @@ public class TntWorld implements Listener {
                 if (this.playerCheck()==1){
                     this.gameClear();
                 }
-                if (this.playerCheck()==1){
-                    this.gameClear();
-                }
+                this.playerCheck();
                 player.sendTitle("GameOver", "ゲームオーバー", 20, 20, 20);
                 player.teleport(new Location(Bukkit.getWorld("tnt"), -265, 52, 1088));
-                this.playerCheck();
             }
         }
     }
