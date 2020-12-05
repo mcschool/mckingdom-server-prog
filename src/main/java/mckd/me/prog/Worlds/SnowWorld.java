5397450748290feaf91cdd653cc0ca3a929d9eab
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 15:25:33 +0900

    test6

--- src/main/java/mckd/me/prog/Worlds/SnowWorld.java
+++ src/main/java/mckd/me/prog/Worlds/SnowWorld.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
@@ -29,14 +30,22 @@ public class SnowWorld implements Listener {
     }
 
 
+   /* @EventHandler
+    public void chengeWorld(PlayerChangedWorldEvent e){
+        if (e.getPlayer().getWorld().getName().equals(this.worldName)) {
+            Player player = e.getPlayer();
+        }
+    }*/
+
+
     public static void hitPlayer(Player player){
         for (int i=0;i<10;i++) {
             Random r = new Random();
             int x = r.nextInt(15);
             int z = r.nextInt(15);
-            Location location = new Location(Bukkit.getWorld("snow"),507, 50, 630);
+            Location location = new Location(Bukkit.getWorld("snow"),507, 20, 630);
             location.add(x,0,z);
-            location.getWorld().spawnArrow(location,new Vector(0,-90,0),0.2f,0);
+            location.getWorld().spawnArrow(location,new Vector(0,0,0),0.2f,0);
             //spawnLocation.getWorld().spawnArrow(spawnLocation, new Vector(x, -1, z), 0.2f,8);
         }
     }
