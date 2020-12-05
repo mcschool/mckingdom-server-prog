a646cf017a78200ce5e89a29091ee6855b1230bd
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 15:10:07 +0900

    test3

--- src/main/java/mckd/me/prog/Worlds/SnowWorld.java
+++ src/main/java/mckd/me/prog/Worlds/SnowWorld.java
@@ -30,12 +30,14 @@ public class SnowWorld implements Listener {
 
 
     public static void hitPlayer(Player player){
-        Location spawnLocation = player.getLocation().clone().add(0, 6, 0);
         for (int i=0;i<10;i++) {
             Random r = new Random();
             int x = r.nextInt(150);
             int z = r.nextInt(150);
-            spawnLocation.getWorld().spawnArrow(spawnLocation, new Vector(x, -1, z), 0.2f,8);
+            Location location = new Location(Bukkit.getWorld("snow"),507, 6, 630);
+            location.add(x,0,z);
+            location.getWorld().spawnArrow(location,new Vector(0,0,0),0.2f,8);
+            //spawnLocation.getWorld().spawnArrow(spawnLocation, new Vector(x, -1, z), 0.2f,8);
         }
     }
 
