0255efa77087ed84b1c6a6fd2b4228e2fb96b9ce
matoi0317 <matoi.shibanuma@gmail.com>
2021-01-16 14:46:55 +0900

    update

--- src/main/java/mckd/me/prog/Worlds/AnvilWorld.java
+++ src/main/java/mckd/me/prog/Worlds/AnvilWorld.java
@@ -1,5 +1,6 @@
 package mckd.me.prog.Worlds;
 
+import com.sun.tools.javac.jvm.Items;
 import mckd.me.prog.Prog;
 import org.bukkit.*;
 import org.bukkit.block.Block;
@@ -10,13 +11,19 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerChangedMainHandEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerRegisterChannelEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
 
 public class AnvilWorld implements Listener {
     private Prog plugin;
     public String worldName = "anvil";
     public Location StartPlace;
     public Location centerPlace;
+    public boolean isPlaying = false;
 
     public AnvilWorld(Prog plugin) {
         this.plugin = plugin;
@@ -37,6 +44,12 @@ public class AnvilWorld implements Listener {
             player.setPlayerWeather(WeatherType.CLEAR);
             World world = player.getWorld();
             world.setTime(30000);
+            ItemStack itemStack = new ItemStack(Material.ANVIL);
+            ItemMeta itemMeta = itemStack.getItemMeta();
+            itemMeta.setDisplayName("ゲームスタート");
+            itemStack.setItemMeta(itemMeta);
+            player.getInventory().addItem(itemStack);
+
         }
     }
 
@@ -48,6 +61,46 @@ public class AnvilWorld implements Listener {
             if (block.getType() == Material.STONE) {
                 player.sendMessage("anvil1");
                 player.setGameMode(GameMode.CREATIVE);
+            }
+        }
+    }
+
+
+            @EventHandler
+                public void interactBlock(PlayerInteractEvent e) {
+         if (e.getPlayer().getWorld().getName().equals(this.worldName)) {
+             Player player = e.getPlayer();
+             ItemStack item = e.getItem();
+             if (item.getType() == Material.ANVIL) {
+                 int NowPlayerCount = player.getWorld().getPlayers().size();
+                 if (isPlaying == true) {
+                     player.sendMessage("ゲームがおわるまでまってね");
+                 } else {
+                     if (NowPlayerCount >= 1) {
+                         isPlaying = true;
+                         this.startGame();
+                         player.sendTitle("Gamestart", "ゲームスタート",20,20,20);
+                         player.sendMessage("移動するよ");
+                     } else {
+                         isPlaying = false;
+                         player.sendMessage("2人まで待ってね");
+                     }
+                 }
+             }
+
+         }
+                }
+          public void startGame() {
+              World world = Bukkit.getWorld("Anvil");
+              List<Player> players = world.getPlayers();
+              for (Player player : players) {
+                  player.teleport(new Location(Bukkit.getWorld("Anvil"),-520,5,-1292));
+
+
+
+
+
+
             }
 
 
