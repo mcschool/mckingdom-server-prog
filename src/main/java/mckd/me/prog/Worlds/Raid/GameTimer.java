a1a2519cf5d35facc65ac46f8050e63c039220a2
ataru <ataru4101826514@gmail.com>
2021-03-25 22:24:49 +0900

    update spigot

---
+++ src/main/java/mckd/me/prog/Worlds/Raid/GameTimer.java
@@ -0,0 +1,30 @@
+package mckd.me.prog.Worlds.Raid;
+
+import mckd.me.prog.Prog;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+public class GameTimer extends BukkitRunnable {
+    BukkitTask task = null;
+    int time;
+    Prog plugin;
+    public GameTimer(Prog plugin ,int i) {
+        this.time = i;
+        this.plugin = plugin;
+    }
+    public void start(){
+        //task = this.plugin.getServer().getScheduler().runTaskTimer(this, new Timer(this ,5), 0L, 20L);
+    }
+    @Override
+    public void run() {
+        if (time <= 0){
+            //plugin.getServer().broadcastMessage("Start!");
+            //plugin.getServer().getScheduler().cancelTask(task.getTaskId());
+            this.cancel();
+        }else {
+            plugin.getServer().broadcastMessage("" + time);
+        }
+        time--;
+    }
+}
--- src/main/java/mckd/me/prog/Worlds/Raid/Raid.java
+++ src/main/java/mckd/me/prog/Worlds/Raid/Raid.java
@@ -1,48 +1,45 @@
-package mckd.me.prog.Worlds;
+package mckd.me.prog.Worlds.Raid;
 
 import mckd.me.prog.Prog;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitScheduler;
-
-import java.util.List;
 
 public class Raid implements Listener {
     private Prog plugin;
     public String worldName = "raid";
-    public Location startPlace;
+    public Location startPlace;//スタート地点
+    public int LimitTime = 600;//ゲームの制限時間
 
     public Raid(Prog plugin) {
         this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this,plugin);
-        this.startPlace = new Location(Bukkit.getWorld(this.worldName),296,68,469);
+        this.startPlace = new Location(Bukkit.getWorld(this.worldName),297,68,470);
     }
 
-    @EventHandler
+/*    @EventHandler
     public void onBlockBreak(BlockBreakEvent e) {
         Player player = e.getPlayer();
-        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
-        scheduler.scheduleSyncDelayedTask((Plugin) this, new Runnable() {
-            @Override
-            public void run() {
-                Bukkit.broadcastMessage("test");
-            }
-        }, 100);
-    }
+        player.sendMessage("test");
+        BukkitTask task = new GameTimer(this.plugin, LimitTime).runTaskTimer(this.plugin,0,20);
+    }*/
 
     @EventHandler
     public void ChangeWorld(PlayerChangedWorldEvent e) {
-        if (e.getPlayer().getWorld().equals(this.worldName)) {
+        if (e.getPlayer().getWorld().getName().equals(this.worldName)) {
             Player player = e.getPlayer();
             player.teleport(this.startPlace);
+            this.SetChest();
         }
+    }
+    public void SetChest() {
+        World world = Bukkit.getWorld("raid");
+        Location location = new Location(Bukkit.getWorld(this.worldName),297,68,471);
+        world.getBlockAt(location).setType(Material.CHEST);
     }
 }
