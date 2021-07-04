1cd0374938119eed0c64c3ab84c98bae0d903a65
BuildTools <unconfigured@null.spigotmc.org>
2021-07-04 16:01:44 +0900

    test

--- src/main/java/mckd/me/prog/Worlds/Build/BuildScheduler.java
+++ src/main/java/mckd/me/prog/Worlds/Build/BuildScheduler.java
@@ -4,18 +4,18 @@ import mckd.me.prog.Prog;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scoreboard.*;
+import org.bukkit.scheduler.BukkitTask;
 
-public class BuildScheduler extends JavaPlugin {
+public class BuildScheduler implements CommandExecutor {
 
-    private Objective obj;
-    private Scoreboard board;
-
+    private BukkitTask task;
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd,String label,String[] args){
@@ -30,18 +30,7 @@ public class BuildScheduler extends JavaPlugin {
                 return true;
             }
 
-            if(obj == null){
-                board = getServer().getScoreboardManager().getMainScoreboard();
-                if(board.getObjective("objective") != null){
-                    obj = board.getObjective("objective");
-                } else {
-                    obj = board.registerNewObjective("objective","dummy");
-                    obj.setDisplaySlot(DisplaySlot.SIDEBAR);
-                    obj.setDisplayName("時間");
-                    Score space = obj.getScore("");
-                    space.setScore(2);
-                }
-            }
+
             sender.sendMessage("§b" + count + "秒数えます...");
             BukkitRunnable task = new BukkitRunnable() {
                 int i = count;
@@ -50,11 +39,9 @@ public class BuildScheduler extends JavaPlugin {
                     if(i == 0){
                         sender.sendMessage("§aカウントが終了しました");
                         cancel();
-                        removeAllScores();
                         return;
                     }
-                    Score timer = obj.getScore("残り" + i + "秒");
-                    timer.setScore(1);
+                    BossBar bossBar = Bukkit.createBossBar("残り時間 " + i +" 秒", BarColor.BLUE, BarStyle.SEGMENTED_10);
                     sender.sendMessage("カウント: " + i);
                     i--;
                 }
@@ -64,11 +51,5 @@ public class BuildScheduler extends JavaPlugin {
         }
         sender.sendMessage("§c使い方: /count <count>");
         return true;
-    }
-    public void removeAllScores(){
-        for(OfflinePlayer item : board.getPlayers()){
-            obj.getScore(item).setScore(0);
-            board.resetScores(item);
-        }
     }
 }
