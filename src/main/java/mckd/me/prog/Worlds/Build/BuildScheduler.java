a896039b05288851d72b73f6f30d69bc5c685f85
BuildTools <unconfigured@null.spigotmc.org>
2021-07-04 15:33:09 +0900

    test

--- src/main/java/mckd/me/prog/Worlds/Build/BuildScheduler.java
+++ src/main/java/mckd/me/prog/Worlds/Build/BuildScheduler.java
@@ -3,24 +3,22 @@ package mckd.me.prog.Worlds.Build;
 import mckd.me.prog.Prog;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.*;
 
-public class BuildScheduler implements CommandExecutor {
+public class BuildScheduler extends JavaPlugin {
+
+    private Objective obj;
+    private Scoreboard board;
+
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd,String label,String[] args){
-        ScoreboardManager manager = Bukkit.getScoreboardManager();
-        Scoreboard board = manager.getMainScoreboard();
-        Objective obj = board.registerNewObjective("count", "dummy");
-        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
-        obj.setDisplayName("時間");
-        board.resetScores("時間");
-        Score space = obj.getScore("");
-        space.setScore(2);
         if(args.length == 1){
             if(!StringUtils.isNumeric(args[0])){
                 sender.sendMessage("§ccountは数字で指定してください");
@@ -32,6 +30,18 @@ public class BuildScheduler implements CommandExecutor {
                 return true;
             }
 
+            if(obj == null){
+                board = getServer().getScoreboardManager().getMainScoreboard();
+                if(board.getObjective("objective") != null){
+                    obj = board.getObjective("objective");
+                } else {
+                    obj = board.registerNewObjective("objective","dummy");
+                    obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+                    obj.setDisplayName("時間");
+                    Score space = obj.getScore("");
+                    space.setScore(2);
+                }
+            }
             sender.sendMessage("§b" + count + "秒数えます...");
             BukkitRunnable task = new BukkitRunnable() {
                 int i = count;
@@ -40,6 +50,7 @@ public class BuildScheduler implements CommandExecutor {
                     if(i == 0){
                         sender.sendMessage("§aカウントが終了しました");
                         cancel();
+                        removeAllScores();
                         return;
                     }
                     Score timer = obj.getScore("残り" + i + "秒");
@@ -53,5 +64,11 @@ public class BuildScheduler implements CommandExecutor {
         }
         sender.sendMessage("§c使い方: /count <count>");
         return true;
+    }
+    public void removeAllScores(){
+        for(OfflinePlayer item : board.getPlayers()){
+            obj.getScore(item).setScore(0);
+            board.resetScores(item);
+        }
     }
 }
