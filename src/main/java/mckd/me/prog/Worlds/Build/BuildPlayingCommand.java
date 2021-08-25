f264b71cebb6de96c97c1e3b6b5c086905d67e82
BuildTools <unconfigured@null.spigotmc.org>
2021-08-25 14:55:40 +0900

    tsets

--- src/main/java/mckd/me/prog/Worlds/Build/BuildPlayingCommand.java
+++ src/main/java/mckd/me/prog/Worlds/Build/BuildPlayingCommand.java
@@ -12,9 +12,14 @@ public class BuildPlayingCommand {
     public static boolean command(CommandSender sender, Command command, String label, String args[]) {
 
         if (args.length == 1) {
-            if (args[0].equalsIgnoreCase("true") || args[0].equalsIgnoreCase("false")) {
-                sender.sendMessage("aa");
-            } else {
+            sender.sendMessage("1");
+            if (args[0].equalsIgnoreCase("true")) {
+                sender.sendMessage("true1");
+            }
+            if (args[0].equalsIgnoreCase("false")){
+                sender.sendMessage("false1");
+            }
+            else {
                 sender.sendMessage("§ctrueもしくはfalseを入れてください");
                 return true;
             }
