9c27f3365cba6104798c22da42ed2d9ced1a8590
BuildTools <unconfigured@null.spigotmc.org>
2021-08-26 14:38:47 +0900

    test

--- src/main/java/mckd/me/prog/Worlds/Commands/sendTitleCommand.java
+++ src/main/java/mckd/me/prog/Worlds/Commands/sendTitleCommand.java
@@ -14,10 +14,11 @@ public class sendTitleCommand {
             World world = ((Player) sender).getWorld();
             List<Player> players = world.getPlayers();
             for(Player p: players){
-                p.sendTitle(Color.ORANGE + args[0],"",0,40,0);
+                p.sendTitle(Color.ORANGE + "" + args[0],"",0,40,0);
             }
             return true;
         }
+        sender.sendMessage("§c使い方: /sendTitle <表示したい文字>");
         return true;
     }
 }
